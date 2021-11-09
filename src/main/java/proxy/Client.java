4e90f8ec4a50d28faca40d8d8780d0bb5cd832a4
김동구 <bbonggu92@gmail.com>
2021-11-09 22:10:27 +0900

    Add Dynamic Proxy

--- src/main/java/proxy/Client.java
+++ src/main/java/proxy/Client.java
@@ -1,8 +1,30 @@
 package proxy;
 
+import java.lang.reflect.Proxy;
+
 public class Client {
     public static void main(String[] args) {
         GameService gameService = new GameServiceProxy(new LoLGameService());
         gameService.startGame();
+
+        Client client = new Client();
+        client.dynamicProxy();
+    }
+
+    private void dynamicProxy() {
+        GameService gameService = this.getGameServiceProxy(new LoLGameService());
+        gameService.startGame();
+    }
+
+    private GameService getGameServiceProxy(GameService target) {
+        return (GameService) Proxy.newProxyInstance(
+                this.getClass().getClassLoader(),
+                new Class[]{GameService.class},
+                (proxy, method, args) -> {
+                    System.out.println("dynamic proxy start");
+                    method.invoke(target, args);
+                    System.out.println("dynamic proxy end");
+                    return null;
+                });
     }
 }
